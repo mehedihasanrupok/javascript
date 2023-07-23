87b470ad561c04748cef1d3412717f124ab648c3
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-07-23 10:32:52 +0600

    first commit

---
+++ Math.js
@@ -0,0 +1,26 @@
+var num1 = 20;
+var num2 = 30;
+// console.log(num1+num2);
+
+var total =500;
+var each = 50;
+var eachGot = total/each;
+// console.log(eachGot);
+
+eachGot+=5;
+// console.log(eachGot);
+each++;
+// console.log(each);
+
+var firstName ='Mehedi';
+var secondName = 'Hasan';
+var fullName = firstName + ' '+ secondName;
+// console.log(fullName);
+
+//convert string to number
+var num1 = 20;
+var num2 = '30';
+var total2number = parseInt(num2);
+var total2 = num1 + total2number;
+console.log(total2);
+console.log(typeof total2number);
