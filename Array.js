850a170371aa22e892fb1a7e9c43b128d093b19e
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-07-24 11:33:12 +0600

    Array

---
+++ Array.js
@@ -0,0 +1,10 @@
+var friendAge =[10,12,11,13,9];
+console.log(friendAge);
+// console.log(friendAge.length);
+
+// console.log(friendAge[3]);
+friendAge[3]= 55;
+console.log(friendAge);
+
+var index = friendAge.indexOf(12);
+console.log(index);
