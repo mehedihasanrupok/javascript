5b115a9c3faf48f20a1cd2f16ac17cde5ead841b
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-07-25 11:48:15 +0600

    Loop

---
+++ Loops.js
@@ -0,0 +1,15 @@
+var roastGiven =0;
+
+while(roastGiven < 7){
+    // console.log('Diye Den');
+    roastGiven++;
+}
+
+for(var given = 0 ; given <7; given++){
+    // console.log('Di Den');
+}
+
+var numbers = [10, 22, 32, 23, 45];
+for(var i= 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
